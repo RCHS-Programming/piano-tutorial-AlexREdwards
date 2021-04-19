16d2823b1dea398f052ae40a50b9e806ae99553b
AlexREdwards <alexander.edwards@stu.boone.kyschools.us>
2021-04-19 08:39:08 -0400

    key

--- Key.java
+++ Key.java
@@ -2,11 +2,20 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, and Greenfoot)
 
 public class Key extends Actor
 {
+    private boolean isDown;
+    private String key;
+    private String sound;
     /**
      * Create a new key.
      */
     public Key()
     {
+        isDown = false;
+    }
+    public Key(String keyName, String soundFile)
+    {
+        key = keyName;
+        sound = soundFile;
     }
 
     /**
@@ -14,5 +23,20 @@ public class Key extends Actor
      */
     public void act()
     {
+         if ( !isDown && Greenfoot.isKeyDown (key))
+         {
+              setImage ("white-key-down.png"); 
+              play ();
+              isDown = true;
+         }
+         if(isDown && !Greenfoot.isKeyDown (key))
+         {
+               setImage ("white-key.png");
+               isDown = false;
+         }
+    }
+    public void play()
+    {
+        Greenfoot.playSound(sound);
     }
 }
